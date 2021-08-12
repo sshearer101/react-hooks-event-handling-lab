3b918c1a6b49997b0d6573aec858ecf1d52aff03
Sam Shearer <sshearer101@gmail.com>
2021-08-11 18:59:48 -0600

    commit tree c7fd5fedf52e1e2ce9932a650ddb8aa022919688

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focus(){
+        console.log('Good!')
+
+    }
+    function blur (){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+            <button onFocus={focus} onBlue={blur}> Eyes on me </button>
+    )
+}
+
+
+
+export default EyesOnMe
